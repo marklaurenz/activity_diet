9251cebee52f7e5775db7455cbc8d6e2e4920796
Mark Laurenz <m-laurenz@kellogg.northwestern.edu>
2014-06-12 17:25:38 -0500

    rand change

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,7 +3,7 @@ class IdeasController < ApplicationController
     @ideas = Idea.all
     # @solution = Activity.all.sample(1)
     # find an activity at random from the activity list, get that activities image_url and description, report the image and then description
-    @solution = Activity.where( :id => rand(1..Activity.count))
+    @solution = Activity.offset(rand(Activity.count))
     @url = @solution.first.website
   end
 
